30ce9ff644ea8100b3273aedba179380c8b8410f
RainyForest23 <wrim0923@gmail.com>
2026-05-30 21:46:21 +0900

    feat: [REQ15] Main.java 메뉴 틀 및 menu/ 스켈레톤 파일 생성
    - Main.java: 12개 메뉴 case + 각 REQ/담당자 주석 연결
    - src/menu/: 12개 메뉴 클래스 스켈레톤 (REQ·담당자 주석 포함)

---
+++ src/menu/InsertSubscriptionMenu.java
@@ -0,0 +1,20 @@
+package menu;
+
+import util.DBUtil;
+import java.sql.*;
+import java.util.Scanner;
+
+/**
+ * [REQ5②] INSERT② 구독 등록
+ * 담당: 이태영
+ * - 회원 ID, 플랜 ID 입력 → subscription 테이블 INSERT
+ * - region_snapshot: 입력 시점 member.region 스냅샷 저장 [REQ14]
+ * - PreparedStatement 사용 [REQ10]
+ */
+public class InsertSubscriptionMenu {
+
+    public void run(Scanner scanner) {
+        // TODO [이태영]: 구현
+        System.out.println("미구현 - INSERT② 구독 등록");
+    }
+}
---
+++ src/menu/SelectMemberBillingMenu.java
@@ -0,0 +1,20 @@
+package menu;
+
+import util.DBUtil;
+import java.sql.*;
+import java.util.Scanner;
+
+/**
+ * [REQ6②] SELECT② 회원별 구독·결제 이력 조회
+ * 담당: 하지수
+ * - 회원 ID 입력 → member_billing_summary_view JOIN member 조회
+ * - VIEW + JOIN 사용 [REQ6]
+ * - PreparedStatement 사용 [REQ10]
+ */
+public class SelectMemberBillingMenu {
+
+    public void run(Scanner scanner) {
+        // TODO [하지수]: 구현
+        System.out.println("미구현 - SELECT② 회원별 구독·결제 이력 조회");
+    }
+}
